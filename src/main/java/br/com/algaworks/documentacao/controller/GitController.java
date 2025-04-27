f4cb6cecd8ad57f1d5fec3988c69ed357e0dd97e
jacksonrocha <jackson_lbr@hotmail.com>
2025-04-27 20:48:42 -0300

    Criando os caminhos

--- src/main/java/br/com/algaworks/documentacao/controller/GitController.java
+++ src/main/java/br/com/algaworks/documentacao/controller/GitController.java
@@ -6,8 +6,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GitController {
 
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/work")
+    public String work() {
+        return "work";
+    }
+
+    @GetMapping("/hobby")
+    public String hobby() {
+        return "hobby";
+    }
+
+    @GetMapping("/develop")
+    public String develop() {
+        return "develop";
+    }
+
+    @GetMapping("/house")
+    public String house() {
+        return "house";
+    }
+
+    @GetMapping("/finances")
+    public String finances() {
+        return "finances";
+    }
+
     @GetMapping("/git")
     public String gitPage() {
         return "git";
     }
 }
+
